00cd621293db186ea09d8ccb059de9bac3b04e81
marcozecchini <zecchini@diag.uniroma1.it>
2023-10-08 00:02:19 +0200

    scale

--- zkp_interface/create_image.py
+++ zkp_interface/create_image.py
@@ -1,7 +1,7 @@
 from PIL import Image
 
-# Create a new blank image with a red pixel (1x1 pixel, RGBA format)
-image = Image.new("RGBA", (1, 1), (255, 0, 0, 255))
+# Create a new blank image with red pixels (RGBA format)
+image = Image.new("RGBA", (16, 16), (255, 0, 0, 255))
 
 # Save the image as a PNG file without compression
 image.save("red_pixel.png", "PNG", compress_level=0)
