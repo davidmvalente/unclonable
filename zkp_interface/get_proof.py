09125440b5b5521a59922404cd8c46dcafce11bb
Merybria99 <maria.rosariabriglia99@gmail.com>
2023-10-07 18:23:10 +0200

    argparser

--- zkp_interface/get_proof.py
+++ zkp_interface/get_proof.py
@@ -1,14 +1,14 @@
 
 from scripts.utils import generate_circuit, get_ppm_parameters
-
+import argparse
 # python3 src/get_proof.py to generate the input JSON file and circuit file
 if __name__ == '__main__':
-    photo = './zkp_interface/scripts/sakura.png'
-    output = 'sakura.json'
-
-    
-
-    json, width, height, length, header_len = get_ppm_parameters(photo, output)
+    #parse arguments 
+    parser = argparse.ArgumentParser(description='Generate input JSON file and circuit file')
+    parser.add_argument('png', type=str, help='path to the photo')
+    parser.add_argument('output', type=str, help='path to the output JSON file')
+    args = parser.parse_args()
+    json, width, height, length, header_len = get_ppm_parameters(args.png, args.output)
     
     circuit_path = generate_circuit({'Nt': width, 'Mt': height, 'SIZEt': length,'HEADERt': header_len},
                                      './zkp_interface/circuits/base/pngToRgb.circom')
